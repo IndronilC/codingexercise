93da922396e819e59e0bfc213ceefe00c1504c2e
Indronil Chakroborty <chakraborty.indranil@gmail.com>
2023-02-25 22:20:46 +0530

    API for get all tweets is now complete

--- src/main/java/com/example/twitterfile/read/codingexercise/controller/TweetsAPIController.java
+++ src/main/java/com/example/twitterfile/read/codingexercise/controller/TweetsAPIController.java
@@ -1,10 +1,13 @@
 package com.example.twitterfile.read.codingexercise.controller;
 
 import com.example.twitterfile.read.codingexercise.dto.TweetResponse;
+import com.example.twitterfile.read.codingexercise.dto.TweetsDTO;
 import com.example.twitterfile.read.codingexercise.service.JsonFileReadService;
+import com.example.twitterfile.read.codingexercise.service.TweetsService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -15,23 +18,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/api/file/v1")
 @RequiredArgsConstructor
 @Slf4j
 public class TweetsAPIController {
     @Autowired
-    JsonFileReadService jsonFileReadService;
-    @Value("classpath:json/tweets.json")
-    Resource resourceFile;
-
+    TweetsService tweetsService;
     @Autowired
-    ObjectMapper objectMapper;
+    ModelMapper modelMapper;
 
     @GetMapping("/tweets")
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<TweetResponse> getAllTweets()  {
-       return null;
+    public ResponseEntity<List<TweetResponse>> getAllTweets()  {
+        List<TweetsDTO> tweetsDTOList = tweetsService.getAllTweets();
+        List<TweetResponse> tweetResponseList = tweetsDTOList.stream()
+                .map(tweetsDTO -> modelMapper.map(tweetsDTO, TweetResponse.class))
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(tweetResponseList, HttpStatus.OK);
     }
 
 
--- src/main/java/com/example/twitterfile/read/codingexercise/dto/TweetResponse.java
+++ src/main/java/com/example/twitterfile/read/codingexercise/dto/TweetResponse.java
@@ -1,4 +1,21 @@
 package com.example.twitterfile.read.codingexercise.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class TweetResponse {
+    @JsonProperty("created_at")
+    private String createdAt;
+
+    @JsonProperty("id_str")
+    private String idStr;
+    private String text;
+    private String source;
 }
