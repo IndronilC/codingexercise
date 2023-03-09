c509ce59ded76b36d22536928e549252ed37a5bb
Indronil Chakroborty <chakraborty.indranil@gmail.com>
2023-03-09 23:35:23 +0530

    Added Cross Origin annotation to call my UI application based on React JS for the same

--- src/main/java/com/example/twitterfile/read/codingexercise/controller/TweetsAPIController.java
+++ src/main/java/com/example/twitterfile/read/codingexercise/controller/TweetsAPIController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -22,6 +23,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 @RequestMapping("/api/file/v1")
 @RequiredArgsConstructor
 @Slf4j
